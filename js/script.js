26d51f74bdadd5a54cee19b1330d871f0f9bbd6a
Matteo0421 <matteo.carola99@gmail.com>
2024-03-11 17:26:48 +0100

    [error]print

--- js/script.js
+++ js/script.js
@@ -10,15 +10,49 @@ createApp({
       messaggio: 'Ciaoooo, primo giorno con Vue.jssss',
       img: './img/01.webp',
 
+      lista : [
+        {
+          alunno: 'Matteo',
+          eta : 20,
+          sesso : 'maschio',
+        },
+        {
+          alunno: 'Mart',
+          eta : 20,
+          sesso : 'femmina',
+        },
+        {
+          alunno: 'Ciro',
+          eta : 20,
+          sesso : 'maschio',
+        },
+        {
+          alunno: 'Lisa',
+          eta : 24,
+          sesso : 'femmina',
+        }
+      ],
+
+      ora: '',
     }
+
+    
   },
 
   // si scrivono tutte le funzioni
   methods:{
 
-    // nomefunzione(){
-    //   corpo funzione
-    // }
+    orologio(){
+      const d = new Data();
+      const ora = d.getHours() < 10 ? '0'+ d.getHours() : d.getHours();
+      const minuto = d.getMinutes() < 10 ? '0'+ d.getMinutes() : d.getMinutes();
+      const secondo = d.getSeconds() < 10 ? '0'+ d.getSeconds() : d.getHours();
+      this.ora=`${ora} : ${minuto} : ${secondo}`
+    }
+  },
+
+  mounted(){
+    this.orologio();
   }
 
 }).mount('#app');
